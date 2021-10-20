2a0f9ca13720147eb969826bb9eec452fd1eca6d
George Kouros <george.kouros.ece@gmail.com>
2021-10-20 21:10:32 +0200

    Remove redundant command

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 git clone https://github.com/Angtian/NeMo.git
 cd NeMo
-conda deactivate
 conda create -n nemo python=3.7
 conda activate nemo
 conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch

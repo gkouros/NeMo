c50326a385ccad455123baf77e5c622996922830
George Kouros <george.kouros.ece@gmail.com>
2021-12-14 13:52:35 +0100

    Remove whitespaces

--- PrepareData.sh
+++ PrepareData.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 get_abs_filename() {
-  # $1 : relative filename
-  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+    # $1 : relative filename
+    echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
 
 ROOT=$(get_abs_filename "./")
 
-ENABLE_OCCLUDED=false
+ENABLE_OCCLUDED=true
 DATAROOT="${ROOT}/data"
 
 PATH_PASCAL3DP="${DATAROOT}/PASCAL3D+_release1.1/"
@@ -57,37 +57,34 @@ if ${ENABLE_OCCLUDED}; then
     fi
 fi
 
-
 ####################################################################################################
-# Run dataset creator
+Run dataset creator
 echo "Create raw Pascal3D+ dataset!"
-python ./code/dataset/CreatePascal3DNeMo.py --overwrite False --source_path "${PATH_PASCAL3DP}" \
+python3 ./code/dataset/CreatePascal3DNeMo.py --overwrite False --source_path "${PATH_PASCAL3DP}" \
         --save_path_train "${PATH_CACHE_TRAINING_SET}" --save_path_val "${PATH_CACHE_TESTING_SET}"
 
 if ${ENABLE_OCCLUDED}; then
     for OCC_LEVEL in "${OCC_LEVELS[@]}"; do
-        echo "Create OccludedPascal3D+ dataset!"
-        python ./code/dataset/CreatePascal3DNeMo.py --overwrite False \
+        echo "Create OccludedPascal3D+ dataset L$OCC_LEVEL"
+        python3 ./code/dataset/CreatePascal3DNeMo.py --overwrite False \
                 --source_path "${PATH_PASCAL3DP}" --data_pendix "${OCC_LEVEL}" \
                 --save_path_train "" --save_path_val "${PATH_CACHE_TESTING_SET_OCC}" \
                 --occ_data_path "${PATH_OCCLUDED_PASCAL3DP}"
     done
 fi
 
-
 ####################################################################################################
 # Process meshes
 for MESH_D in "${MESH_DIMENSIONS[@]}"; do
-    python ./tools/CreateCuboidMesh.py --CAD_path "${PATH_PASCAL3DP}" --mesh_d "${MESH_D}"
+    python3 ./tools/CreateCuboidMesh.py --CAD_path "${PATH_PASCAL3DP}" --mesh_d "${MESH_D}"
 done
-
 
 ####################################################################################################
 # Create 3D annotations
 for MESH_D in "${MESH_DIMENSIONS[@]}"; do
-    python ./code/dataset/generate_3Dpascal3D.py --overwrite False \
+    python3 ./code/dataset/generate_3Dpascal3D.py --overwrite False \
             --root_path "${PATH_CACHE_TRAINING_SET}" --mesh_path "${PATH_PASCAL3DP}" --mesh_d "${MESH_D}" &
-    python ./code/dataset/generate_3Dpascal3D.py --overwrite False \
+    python3 ./code/dataset/generate_3Dpascal3D.py --overwrite False \
             --root_path "${PATH_CACHE_TESTING_SET}" --mesh_path "${PATH_PASCAL3DP}" --mesh_d "${MESH_D}" &
 done
 
@@ -98,9 +95,9 @@ wait
 if ${ENABLE_OCCLUDED}; then
     for MESH_D in "${MESH_DIMENSIONS[@]}"; do
         for OCC_LEVEL in "${OCC_LEVELS[@]}"; do
-            python ./code/dataset/link_annotations.py --source_path "${PATH_CACHE_TESTING_SET}" --target_path "${PATH_CACHE_TESTING_SET_OCC}" \
+            python3 ./code/dataset/link_annotations.py --source_path "${PATH_CACHE_TESTING_SET}" --target_path "${PATH_CACHE_TESTING_SET_OCC}" \
                     --occ_level "${OCC_LEVEL}" --mesh_d "${MESH_D}"
-            python ./code/dataset/refine_list.py --root_path "${PATH_CACHE_TESTING_SET_OCC}" \
+            python3 ./code/dataset/refine_list.py --root_path "${PATH_CACHE_TESTING_SET_OCC}" \
                     --occ_level "${OCC_LEVEL}" --mesh_d "${MESH_D}"
         done
     done

72a4a70bfd625113f1401fe21ee2b2813027ca2d
George Kouros <george.kouros.ece@gmail.com>
2022-05-13 15:06:59 +0200

    Add 50-50 train-val split of training data for KITTI3D

--- code/dataset/CreateKITTI3D.py
+++ code/dataset/CreateKITTI3D.py
@@ -3,28 +3,29 @@ from PIL import Image
 import os
 import BboxTools as bbt
 import cv2
+from tqdm.auto import tqdm
+from math import radians
 
+split = 'val'
 
-split = 'training'
-
-annotation_path = './%s/label/' % split
-image_path = './%s/image/' % split
-calib_path = './%s/calib/' % split
-
+data_dir = '/esat/topaz/gkouros/datasets/KITTI3D'
+annotation_path = '%s/%s/label_2/' % (data_dir, 'training')
+image_path = '%s/%s/image_2/' % (data_dir, 'training')
+calib_path = '%s/%s/calib/' % (data_dir, 'training')
+splits_path = '%s/mv3d_kitti_splits' % (data_dir)
 occ_level_mapping = {0: 'fully_visible', 1: 'partly_occluded', 2: 'largely_occluded', 3: 'unknown'}
 
-save_dir = {'training': 'KITTI_train_distcrop', 'testing': 'KITTI_distcrop'}[split]
+save_dir = {
+    'train': '%s/KITTI3D_train_NeMo' % data_dir,
+    'val': '%s/KITTI3D_val_NeMo' % data_dir,
+    # 'test': '%s/KITTI_test_NeMo' % data_dir,  # there are no labels for the test set
+}[split]
+
 cate = 'car'
-
 cate_map = {'car': 'Car'}
-resize_to = 224
-
-min_box_size = 100
-
+min_box_size = 40
 camera_matrix_num = 2
-
 out_shape = (256, 672)
-
 elevation_default = 0
 theta_default = 0
 
@@ -40,7 +41,9 @@ def get_one_anno(anno_line):
 def if_legal(input_, min_size=40, truncated_max=0.2):
     cate_, occ_level, box_np, truncated = input_
     return cate_ == cate_map[cate] and min(
-        bbt.from_numpy(box_np, sorts=['y0', 'x0', 'y1', 'x1']).shape) > min_size and truncated < truncated_max and occ_level == this_occ_lv
+        bbt.from_numpy(box_np, sorts=['y0', 'x0', 'y1', 'x1']).shape) > min_size \
+        and truncated < truncated_max \
+        and occ_level == this_occ_lv
 
 
 def project_to_2d(x3d, P):
@@ -59,6 +62,11 @@ def project_to_2d(x3d, P):
     else:
         return x2d[:, 0:2]
 
+def read_split_file(path):
+    with open(path, 'r') as f:
+        split = [row.rstrip('\n') for row in f.readlines()]
+    return split
+
 
 if __name__ == '__main__':
     for this_occ_lv in range(3):
@@ -72,43 +80,66 @@ if __name__ == '__main__':
         os.makedirs(save_img_dir, exist_ok=True)
         os.makedirs(save_list_dir, exist_ok=True)
 
-        name_list = [t.split('.')[0] for t in os.listdir(annotation_path)]
+        # read split file
+        name_list = read_split_file(os.path.join(splits_path, split + '.txt'))
+
+        # get filenames in annotations directory
+        # name_list = [t.split('.')[0] for t in os.listdir(annotation_path)]
+        # print(name_list[:10])
 
         out_name = []
-        for this_name in name_list:
+        # loop over all annotations
+        for this_name in tqdm(name_list, desc=occ_level_mapping[this_occ_lv]):
+            # check if an instance is missing an annotation of image file
             if not (os.path.exists(os.path.join(image_path, this_name + '.png'))
                 and os.path.exists(os.path.join(annotation_path, this_name + '.txt'))
                 and os.path.exists(os.path.join(calib_path, this_name + '.txt'))):
                 print('Miss Image or annotation on', this_name)
                 continue
-            image = np.array(Image.open(os.path.join(image_path, this_name + '.png')))
-            annotations = open(os.path.join(annotation_path, this_name + '.txt')).readlines()
-            camera_matrix_txt = open(os.path.join(calib_path, this_name + '.txt')).readlines()[camera_matrix_num]
 
+            # read image
+            image = np.array(Image.open(os.path.join(image_path, this_name + '.png')))
+
+            # read annotation
+            annotations = open(os.path.join(annotation_path, this_name + '.txt')).readlines()
+
+            # read camera matrix
+            camera_matrix_txt = open(os.path.join(calib_path, this_name + '.txt')).readlines()[camera_matrix_num]
             camera_matrix = np.array(camera_matrix_txt.strip().split(' ')[1::], dtype=np.float32).reshape((3, 4)).T
 
+            # get bbox masks of image
             legal_mask = [if_legal(get_one_anno(t), min_size=min_box_size) for t in annotations]
 
             if sum(legal_mask) == 0:
                 continue
 
+            # loop over all instances in image
             for i, annotation in enumerate(annotations):
                 if not legal_mask[i]:
                     continue
+                # get category, occlusion level, and bbox of instance
                 cate_, occ_level, box_np, _ = get_one_anno(annotation)
                 box = bbt.from_numpy(box_np, image_boundary=image.shape[0:2], sorts=('x0', 'y0', 'x1', 'y1'))
                 # Image.fromarray(box.apply(image)).show()
                 annotation = annotation.split(' ')
-
+                # calculate distance of object based on 3d location of instance
                 distance_ = np.sum(np.array(annotation[11:14], dtype=np.float32) ** 2) ** .5
+                # get 3d instance center
                 c3d = np.array(annotation[11:14], dtype=np.float32) - .5 * np.array([0, float(annotation[8]), 0])
+                # get image-plane instance center
                 c2d = project_to_2d(c3d, camera_matrix)
 
+                # calculate azimuth / local angle
                 if np.arctan(c3d[2] / c3d[0]) > 0:
                     azimuth = float(annotation[14]) + np.pi + np.arctan(c3d[2] / c3d[0])
                 else:
                     azimuth = (float(annotation[14]) + np.arctan(c3d[2] / c3d[0])) % (2 * np.pi)
+                # print(annotations)
+                # print(azimuth, float(annotation[3]))
+                # continue
+                # asd
 
+                # calculate camera elevation
                 elevation = -np.arcsin(c3d[1] / distance_)
 
                 resize_rate = float(200 * distance_ / 1280)
@@ -174,9 +205,9 @@ if __name__ == '__main__':
                 save_parameters['cad_index'] = 1
 
                 np.savez(os.path.join(save_anno_dir, this_name + '_%d.npz' % i), **save_parameters)
-                Image.fromarray(img_cropped).save(os.path.join(save_img_dir, this_name + '_%d.JPEG' % i))
+                Image.fromarray(img_cropped).save(os.path.join(save_img_dir, this_name + '_%d.jpg' % i))
 
-                print('Finished: ' + this_name + '_%d.npz' % i)
+                # print('Finished: ' + this_name + '_%d.npz' % i)
                 out_name.append(this_name + '_%d\n' % i)
 
         with open(os.path.join(save_list_dir, cate + '_%s.txt' % occ_level_mapping[this_occ_lv]), 'w') as file_handel:
